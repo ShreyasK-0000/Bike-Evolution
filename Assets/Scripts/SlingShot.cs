87edfbe44d2dd369875951336f6ffd1c06057b35
ShreyasK-0000 <shreyas@nexsteadgames.com>
2026-05-14 22:44:02 +0530

    Prototype of projectile launch using slingshot, Camera Follow

---
+++ Assets/Scripts/SlingShot.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class SlingShot : MonoBehaviour
+{
+
+    [SerializeField] private float maxPullDistance = 2f;
+    [SerializeField] private float shotForce = 10f;
+
+    [SerializeField] private Transform leftPoint;
+    [SerializeField] private Transform rightPoint;
+
+    [SerializeField] public Transform projectile;
+    [SerializeField] private Transform drawFrom;
+
+    [SerializeField] private LineRenderer elasticBand;
+
+    [SerializeField] private Rigidbody rigidBodyOfProjectile;
+
+    [SerializeField] private LayerMask bikePrefabMask;
+
+    public enum State { elasticBandIdle, elasticBandDragging, elasticBandReleased}
+    public State gameState = State.elasticBandIdle;
+
+    private Plane dragPlane;
+
+    private void Start()
+    {
+        ResetBand();
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))  
+        {
+            FullReset();
+        }
+        switch (gameState)
+        {
+            case State.elasticBandIdle: HandleIdle(); break;
+            case State.elasticBandDragging: HandleDragging(); break;
+        }
+    }
+
+
+    private void HandleIdle()
+    {
+        if (!Input.GetMouseButtonDown(0)) return;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, bikePrefabMask))
+        {
+            dragPlane = new Plane(Vector3.up, drawFrom.position);
+            gameState = State.elasticBandDragging;
+            rigidBodyOfProjectile.isKinematic = true; 
+        }
+    }
+
+
+    private void HandleDragging()
+    {
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            LaunchBike();
+            return;
+        }
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (!dragPlane.Raycast(ray, out float enter)) return;
+
+        Vector3 worldPoint = ray.GetPoint(enter);
+
+        worldPoint.y = drawFrom.position.y;
+
+        Vector3 offset = worldPoint - drawFrom.position;
+        if (offset.magnitude > maxPullDistance)
+            offset = offset.normalized * maxPullDistance;
+
+        projectile.position = drawFrom.position + offset;
+
+
+        Vector3 toCenter = drawFrom.position - projectile.position;
+        if (toCenter != Vector3.zero)
+            projectile.forward = toCenter.normalized;
+
+      
+        UpdateBand();
+    }
+
+   
+
+    private void LaunchBike()
+    {
+       
+        Vector3 launchDir = (drawFrom.position - projectile.position).normalized;
+
+      
+        float stretch = Vector3.Distance(projectile.position, drawFrom.position);
+        float forceMagnitude = stretch * shotForce;
+
+        rigidBodyOfProjectile.isKinematic = false;
+        rigidBodyOfProjectile.AddForce(launchDir * forceMagnitude, ForceMode.Impulse);
+
+        gameState = State.elasticBandReleased;
+        ResetBand();
+    }
+
+    
+
+    private void UpdateBand()
+    {
+        elasticBand.SetPositions(new Vector3[3]
+        {
+            leftPoint.position,
+            projectile.position,   
+            rightPoint.position
+        });
+    }
+
+    private void ResetBand()
+    {
+        projectile.position = drawFrom.position;
+        elasticBand.SetPositions(new Vector3[3]
+        {
+            leftPoint.position,
+            drawFrom.position,
+            rightPoint.position
+        });
+        
+    }
+
+
+    private void FullReset()
+    {
+        rigidBodyOfProjectile.isKinematic = true;
+        rigidBodyOfProjectile.linearVelocity = Vector3.zero;
+        rigidBodyOfProjectile.angularVelocity = Vector3.zero;
+
+        projectile.position = drawFrom.position;
+        projectile.rotation = drawFrom.rotation;  
+
+        elasticBand.SetPositions(new Vector3[3]
+        {
+        leftPoint.position,
+        drawFrom.position,
+        rightPoint.position
+        });
+
+        rigidBodyOfProjectile.isKinematic = false;
+        gameState = State.elasticBandIdle;
+    }
+}
